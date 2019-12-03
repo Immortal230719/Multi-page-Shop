b3968789914b14fc66d969342dbfa074fcef705c
Immortal230719 <golub010919@mail.ru>
2019-12-03 21:57:17 +0400

    Slider template added

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
+//= lib/slick.min.js
+
+$(document).ready(function() {
+  // promo-slider
+  $(".promo_slider").slick({
+    dots: true,
+    infinite: true,
+    speed: 1200,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+    ]
+  });
+});
+
 //= partials/header.js

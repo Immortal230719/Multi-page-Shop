d676dc9b63f1396d16957a977c46f7408d12a539
Immortal230719 <golub010919@mail.ru>
2019-12-07 10:49:58 +0400

    template footer & subscribe done

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 
 //= lib/slick.min.js
 //= lib/products.js
+//= lib/wow.min.js
 
 $(document).ready(function() {
   // promo-slider
@@ -22,6 +23,44 @@ $(document).ready(function() {
       }
     ]
   });
+
+  // reviews_carousel
+  $(".reviews_carousel").slick({
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: `<button type="button" class="reviews_carousel_next">></button>`,
+    prevArrow: `<button type="button" class="reviews_carousel_prev"><</button>`,
+    responsive: [
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  // Page UP
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1000) {
+      $(".page-up").fadeIn();
+    } else {
+      $(".page-up").fadeOut();
+    }
+  });
+
+  $("a[href^=#up]").click(function() {
+    const _href = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+    return false;
+  });
+
+  new WOW().init();
 });
 
 //= partials/header.js
